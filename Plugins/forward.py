af2b1cba3f560d321e3d4bc9da3efa9c358cb9df
rahul torat <lorena203232@proton.me>
2024-03-03 21:47:49 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -14,8 +14,6 @@ async def forward(client, message):
       for id in Config.CHANNEL:
          from_channel, to_channel = id.split(":")
          if message.chat.id == int(from_channel):
-            func = message.copy if AS_COPY else message.forward
-            await func(int(to_channel), as_copy=True)
             logger.info("Forwarded a message from", from_channel, "to", to_channel)
             await asyncio.sleep(1)
    except Exception as e:
